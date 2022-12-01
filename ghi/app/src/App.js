dc838737e82fb218ffdfafeaf440a5237b536651
kayre <kayresantos@gmail.com>
2022-11-30 23:49:07 -0800

    implemented react

---
+++ ghi/app/src/App.js
@@ -0,0 +1,45 @@
+import Nav from './Nav';
+import AttendeesList from './AttendeesList';
+import LocationForm from './LocationForm';
+import ConferenceForm from './ConferenceForm';
+import AttendConferenceForm  from './AttendConferenceForm';
+
+// function App(props) {
+//   if (props.attendees === undefined) {
+//     return null;
+//   }
+//   return (
+//     <>
+//       <Nav />
+//       <div className="container">
+//         {/* <AttendConferenceForm/> */}
+//         {/* <ConferenceForm /> */}
+//         {<LocationForm />}
+
+//         {/* <AttendeesList attendees={props.attendees} /> */}
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
+function App(props) {
+  if (props.attendees === undefined) {
+    return null;
+  }
+  return (
+    <BrowserRouter>
+      <Nav />
+      <div className="container">
+          <Routes>
+            <Route path="conferences/new" element={<ConferenceForm />} />
+            <Route path="attendees/new" element={<AttendConferenceForm />} />
+            <Route path="locations/new" element={<LocationForm />} />
+            <Route path="attendees" element={<AttendeesList attendees={props.attendees} />} />
+          </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+export default App
--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -16,16 +17,15 @@ root.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-
 async function loadAttendees() {
   const response = await fetch('http://localhost:8001/api/attendees/');
   if (response.ok) {
     const data = await response.json();
     root.render(
       <React.StrictMode>
-        <App attendees={data.attendees} />
-      </React.StrictMode>
-    );
+      <App attendees={data.attendees} />
+    </React.StrictMode>
+    )
   } else {
     console.error(response);
   }
